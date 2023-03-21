52ab919c0d78248da0d08ad9f45ff927723f5292
Pablo Fernández <pablo.fernandez10@estudiant.upf.edu>
2023-03-21 19:58:44 +0100

    Delete TestDisplayMatrix.java

--- TestDisplayMatrix.java
+++
@@ -1,8 +0,0 @@
-package distancematrix;
-public class TestDisplayMatrix {
-    public static void main ( String[ ] args) {
-        DistanceMatrix matrix = new DistanceMatrix ();
-        DisplayMatrix display = new DisplayMatrix (matrix);
-        display.setVisible(true);
-    }
-}
